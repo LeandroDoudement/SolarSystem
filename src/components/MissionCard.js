4325e7aa7e335022c7565309628e9c95324c51a8
LeandroDoudement <leandrodoudement@gmail.com>
2022-12-14 14:40:02 -0300

    styles-part-2

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import '../styles/MissionCard.css';
+import Calendar from '../images/Calendar.png';
+import Location from '../images/Location.png';
+import Flag from '../images/Flag.png';
 
 class MissionCard extends React.Component {
   render() {
@@ -8,13 +11,22 @@ class MissionCard extends React.Component {
     return (
       <div data-testid="mission-card" className="mission-card">
         <p data-testid="mission-name" className="mission-name">{name}</p>
+        <hr className="mission-divisory" />
         <div className="mission-card-half">
-          <p data-testid="mission-year" className="mission-details">{year}</p>
-          <p data-testid="mission-country" className="mission-details">{country}</p>
+
+          <p data-testid="mission-year" className="mission-details">
+            <img src={ Calendar } alt="Calendar icon" className="mission-icon" />
+            {year}
+          </p>
+          <p data-testid="mission-country" className="mission-details">
+            <img src={ Location } alt="Location icon" className="mission-icon" />
+            {country}
+          </p>
           <p
             data-testid="mission-destination"
             className="mission-details"
           >
+            <img src={ Flag } alt="Flag icon" className="mission-icon" />
             {destination}
 
           </p>
